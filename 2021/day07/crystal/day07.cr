b802748d063132c81a02558c18118232c99d0e95
Andrea Manzini <ilmanzo@gmail.com>
2021-12-07 08:53:40 +0100

    day07 in crystal

---
+++ 2021/day07/crystal/day07.cr
@@ -0,0 +1,21 @@
+
+def sum_to(n)
+    n*(n+1)/2
+end
+
+def part1(input)
+    (input.min..input.max).each.map do |i| 
+        input.map { |n| (n-i).abs }.sum 
+    end.min
+end
+
+def part2(input)
+    (input.min..input.max).each.map do |i| 
+        input.map { |n| sum_to((n-i).abs) }.sum 
+    end.min
+end
+
+input=File.read("../input.txt").split(',').map(&.to_i64)
+puts part1 input
+puts part2 input
+
