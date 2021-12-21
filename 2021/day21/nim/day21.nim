3f6ac7489f638a48f451aa48b65ce61326edc549
Andrea Manzini <ilmanzo@gmail.com>
2021-12-21 11:57:29 +0100

    bugfix part1.nim

--- 2021/day21/nim/day21.nim
+++ 2021/day21/nim/day21.nim
@@ -8,9 +8,9 @@ proc roll3(d: var Dice) : int=
     for i in countup(1,3):
         d.rolls+=1
         d.value+=1
-        result+=d.value
         if d.value==101:
             d.value=1
+        result+=d.value
 
 
 var dice: Dice
@@ -20,9 +20,7 @@ var players = [6,8]
 var scores = [0,0]
 
 while scores[0]<1000 and scores[1]<1000:
-    players[turn]=players[turn]+dice.roll3
-    if players[turn]>10:
-        players[turn]=1+players[turn] mod 10
+    players[turn]=1+(players[turn]+dice.roll3-1) mod 10
     scores[turn]+=players[turn]
     turn=1-turn
 
