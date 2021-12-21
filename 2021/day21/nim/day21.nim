e6ca796992446653e9aaae9ede8f909df2fe5b08
Andrea Manzini <ilmanzo@gmail.com>
2021-12-21 12:15:27 +0100

    part1 nim refactoring

--- 2021/day21/nim/day21.nim
+++ 2021/day21/nim/day21.nim
@@ -1,28 +1,21 @@
-
-
-type Dice = object 
-    value : int
-    rolls : int
-
-proc roll3(d: var Dice) : int=
-    for i in countup(1,3):
-        d.rolls+=1
-        d.value+=1
-        if d.value==101:
-            d.value=1
-        result+=d.value
-
-
-var dice: Dice
 var turn=0
-
 var players = [6,8] 
 var scores = [0,0]
+var rolls=0
+var dice=0
 
-while scores[0]<1000 and scores[1]<1000:
-    players[turn]=1+(players[turn]+dice.roll3-1) mod 10
+while true:
+    rolls+=3
+    var n = -1
+    for i in countup(1,3):
+        dice+=1
+        if dice==101:
+            dice=1
+        n+=dice
+    players[turn]=1+(players[turn]+n) mod 10
     scores[turn]+=players[turn]
+    if scores[turn]>=1000:
+        break
     turn=1-turn
 
-echo min(scores)*dice.rolls
-
+echo min(scores)*rolls
