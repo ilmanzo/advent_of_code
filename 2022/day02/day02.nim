345d7f1462971600f34c3fd08cd8a0e188fd2efd
Andrea Manzini <ilmanzo@gmail.com>
2022-12-02 14:50:10 +0100

    day2 nim refactor

--- 2022/day02/day02.nim
+++ 2022/day02/day02.nim
@@ -30,13 +30,13 @@ proc score(other, me: int): int =
     elif win(other) == me:
         result+=6
 
-proc part1(pl1: char, pl2: char): int =
-    score(int(pl1)-A, int(pl2)-X)
+proc part1(pl: seq[char]): int =
+    score(int(pl[0])-A, int(pl[1])-X)
 
 # X = lose, Y=draw, Z=win
-proc part2(pl1: char, pl2: char): int =
-    let other = int(pl1)-A
-    case pl2:
+proc part2(pl: seq[char]): int =
+    let other = int(pl[0])-A
+    case pl[1]:
         of 'X': return score(other, other.lose)
         of 'Y': return score(other, other)
         of 'Z': return score(other, other.win)
@@ -45,8 +45,8 @@ proc part2(pl1: char, pl2: char): int =
 var total1, total2: int
 for line in "input.txt".lines:
     let players = line.split.mapIt(char(it[0]))
-    total1+=part1(players[0], players[1])
-    total2+=part2(players[0], players[1])
+    total1+=players.part1
+    total2+=players.part2
 
 echo "part1=", total1
 echo "part2=", total2
