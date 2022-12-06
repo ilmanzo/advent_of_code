efd8a083a03d9b0306fcddcb5996a298215efb7e
Andrea Manzini <ilmanzo@gmail.com>
2022-12-06 08:38:31 +0100

    day06 nim

---
+++ 2022/day06/day06.nim
@@ -0,0 +1,22 @@
+import std/sets
+
+func isunique(s: string): bool =
+    let s1 = s.toHashSet
+    s1.len == s.len
+
+proc findStartOfPacket(input: string, n: int): int =
+    let start = n-1
+    for i in start..input.len:
+        if isunique(input[i-start..i]):
+            return i+1
+
+let input = readFile("input.txt")
+echo "Part1=", findStartOfPacket(input, 4)
+echo "Part2=", findStartOfPacket(input, 14)
+
+
+
+
+
+
+
