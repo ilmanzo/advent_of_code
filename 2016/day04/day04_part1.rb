ffe18a6d85b04166babde9936a260ba86594b850
Andrea Manzini <ilmanzo@gmail.com>
2022-12-11 10:17:51 +0100

    2016 day4 part1

---
+++ 2016/day04/day04_part1.rb
@@ -0,0 +1,7 @@
+part1=STDIN.each_line.map do |line| 
+    (input,roomid,crc)=line.chomp.scan(/^(.+)-(\d+)\[(\w+)\]/).first
+    freq=input.chars.filter{|c| c!="-"}.tally.sort_by {|k,v| [v,255-k.ord]}.reverse[..4]
+    crc==freq.map{|f|f[0]}.join('') ? roomid.to_i : 0
+end.sum
+
+p part1 
