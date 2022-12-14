62d2b5c95dd4544f9214d41988b2d77576aa9f2e
Andrea Manzini <ilmanzo@gmail.com>
2022-12-14 17:03:51 +0100

    refactor day14

--- 2022/day14/day14_rust/src/main.rs
+++ 2022/day14/day14_rust/src/main.rs
@@ -16,11 +16,33 @@ struct Cave {
 }
 
 impl Cave {
-    fn new() -> Self {
-        Self {
+    fn init_from_input() -> Self {
+        let input = fs::read_to_string("../input.txt").unwrap();
+        let mut cave = Self {
             cells: [[Cell::Air; CAVE_SIZE]; CAVE_SIZE],
             lowest: 0,
+        };
+        let lines: Vec<_> = input.lines().collect();
+
+        for line in lines {
+            let instructions: Vec<_> = line.split(" -> ").collect();
+            for i in 0..instructions.len() - 1 {
+                let p1 = split_on_comma(instructions[i]);
+                let p2 = split_on_comma(instructions[i + 1]);
+                for j in p1[0].min(p2[0])..=p1[0].max(p2[0]) {
+                    for k in p1[1].min(p2[1])..=p1[1].max(p2[1]) {
+                        cave.cells[j][k] = Cell::Rock;
+                    }
+                }
+                if p1[1] > cave.lowest {
+                    cave.lowest = p1[1];
+                }
+                if p2[1] > cave.lowest {
+                    cave.lowest = p2[1];
+                }
+            }
         }
+        cave
     }
 }
 
@@ -28,7 +50,8 @@ fn split_on_comma(s: &str) -> Vec<usize> {
     s.split(',').map(|x| x.parse().unwrap()).collect()
 }
 
-fn part1(cave: &mut Cave) -> usize {
+fn part1() -> usize {
+    let mut cave = Cave::init_from_input();
     let mut count = 0;
     loop {
         let mut x = 500;
@@ -56,11 +79,11 @@ fn part1(cave: &mut Cave) -> usize {
     }
 }
 
-fn part2(cave: &mut Cave) -> usize {
+fn part2() -> usize {
+    let mut cave = Cave::init_from_input();
     for i in 0..cave.cells.len() {
         cave.cells[i][cave.lowest + 2] = Cell::Rock;
     }
-    println!("lowest: {}", cave.lowest);
     let mut count = 0;
 
     loop {
@@ -91,31 +114,21 @@ fn part2(cave: &mut Cave) -> usize {
 }
 
 fn main() {
-    let input = fs::read_to_string("../input.txt").unwrap();
+    println!("Part1: {}", part1());
+    println!("Part2: {}", part2());
+}
 
-    let mut cave = Cave::new();
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
 
-    let lines: Vec<_> = input.lines().collect();
-
-    for line in lines {
-        let instructions: Vec<_> = line.split(" -> ").collect();
-        for i in 0..instructions.len() - 1 {
-            let p1 = split_on_comma(instructions[i]);
-            let p2 = split_on_comma(instructions[i + 1]);
-            for j in p1[0].min(p2[0])..=p1[0].max(p2[0]) {
-                for k in p1[1].min(p2[1])..=p1[1].max(p2[1]) {
-                    cave.cells[j][k] = Cell::Rock;
-                }
-            }
-            if p1[1] > cave.lowest {
-                cave.lowest = p1[1];
-            }
-            if p2[1] > cave.lowest {
-                cave.lowest = p2[1];
-            }
-        }
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(), 672);
     }
-    // TODO: refactor to avoid cloning
-    println!("Part1: {}", part1(&mut cave.clone()));
-    println!("Part2: {}", part2(&mut cave.clone()));
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(), 26831);
+    }
 }
