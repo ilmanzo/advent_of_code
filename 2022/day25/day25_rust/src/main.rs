0abc3df06d3f273398f3cd6c8c5e409eb89f474a
Andrea Manzini <ilmanzo@gmail.com>
2022-12-26 11:02:52 +0100

    2022 day25 traits

--- 2022/day25/day25_rust/src/main.rs
+++ 2022/day25/day25_rust/src/main.rs
@@ -1,49 +1,72 @@
+use core::fmt;
+
 const INPUT: &str = include_str!("../../input.txt");
 
 fn main() {
-    let sum = INPUT.trim_end().lines().map(|l| snafu_to_dec(l)).sum();
-    println!("decimal sum = {} \nsnafu sum = {}", sum, dec_to_snafu(sum));
+    let sum: usize = INPUT
+        .trim_end()
+        .lines()
+        .map(|l| Snafu::from(l))
+        .map(|s| <usize>::from(s))
+        .sum();
+    println!("decimal sum = {} \nsnafu sum = {}", sum, Snafu::from(sum));
 }
 
-// TODO: implement proper traits to manage and display snafu numbers
+#[derive(Debug)]
+struct Snafu(String);
 
-// converts a snafu string into a decimal number
-fn snafu_to_dec(l: &str) -> u64 {
-    let mut result: i64 = 0;
-    for ch in l.chars() {
-        result *= 5;
-        result += match ch {
-            '0' => 0,
-            '1' => 1,
-            '2' => 2,
-            '-' => -1,
-            '=' => -2,
-            _ => panic!("unexpected digit {}", ch),
-        }
+impl fmt::Display for Snafu {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "SNAFU[{}]", self.0)
     }
-    result as u64
 }
 
-// converts a decimal number into a string with snafu notation
-fn dec_to_snafu(mut num: u64) -> String {
-    let mut res = Vec::with_capacity(25);
-    while num > 0 {
-        let digit = (num % 5) as u32;
-        match digit {
-            0 => res.push('0'),
-            1 => res.push('1'),
-            2 => res.push('2'),
-            3 => {
-                res.push('=');
-                num += 2;
-            }
-            4 => {
-                res.push('-');
-                num += 1;
-            }
-            _ => panic!("unexpected digit {}", digit),
-        }
-        num /= 5;
+impl From<&str> for Snafu {
+    fn from(s: &str) -> Self {
+        Snafu(String::from(s))
     }
-    res.into_iter().rev().collect()
+}
+
+impl From<usize> for Snafu {
+    fn from(n: usize) -> Self {
+        let mut num = n;
+        let mut result = Vec::with_capacity(25);
+        while num > 0 {
+            let digit = (num % 5) as u32;
+            match digit {
+                0 => result.push('0'),
+                1 => result.push('1'),
+                2 => result.push('2'),
+                3 => {
+                    result.push('=');
+                    num += 2;
+                }
+                4 => {
+                    result.push('-');
+                    num += 1;
+                }
+                _ => panic!("unexpected digit {}", digit),
+            }
+            num /= 5;
+        }
+        Self(result.into_iter().rev().collect())
+    }
+}
+
+impl From<Snafu> for usize {
+    fn from(s: Snafu) -> Self {
+        let mut result: isize = 0;
+        for ch in s.0.chars() {
+            result *= 5;
+            result += match ch {
+                '0' => 0,
+                '1' => 1,
+                '2' => 2,
+                '-' => -1,
+                '=' => -2,
+                _ => panic!("unexpected digit {}", ch),
+            }
+        }
+        result as usize
+    }
 }
