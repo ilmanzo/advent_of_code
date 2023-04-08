f93d928f3695e1fed0be735648ffbb9eef59c7b6
Andrea Manzini <ilmanzo@gmail.com>
2023-04-08 17:16:57 +0200

    2020 day4 part2

--- 2020/day04/day04_rust/src/lib.rs
+++ 2020/day04/day04_rust/src/lib.rs
@@ -1,20 +1,56 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn get_input(filepath: &str) -> String {
     fs::read_to_string(filepath).expect("can't open input file")
 }
 
-const REQUIRED_FIELDS: [&'static str; 7] = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"];
+const REQUIRED_FIELDS: [&str; 7] = ["byr:", "iyr:", "eyr:", "hgt:", "hcl:", "ecl:", "pid:"];
+const VALID_EYE_COLORS: [&str; 7] = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
 
-fn passport_valid(passport: &str) -> bool {
+// check all required fields are present
+fn passport_valid1(passport: &str) -> bool {
     REQUIRED_FIELDS.iter().all(|field| passport.contains(field))
 }
 
+fn validate_field(key: &str, value: &str) -> bool {
+    match key {
+        "byr" => value.parse::<usize>().unwrap().wrapping_sub(1920) <= 82,
+        "iyr" => value.parse::<usize>().unwrap().wrapping_sub(2010) <= 10,
+        "eyr" => value.parse::<usize>().unwrap().wrapping_sub(2020) <= 10,
+        "hgt" => {
+            if value.ends_with("cm") && value.len() == 5 {
+                value[0..3].parse::<usize>().unwrap().wrapping_sub(150) <= 43
+            } else if value.ends_with("in") && value.len() == 4 {
+                value[0..2].parse::<usize>().unwrap().wrapping_sub(59) <= 27
+            } else {
+                false
+            }
+        }
+        "hcl" => value.len() == 7,
+        "ecl" => VALID_EYE_COLORS.iter().any(|v| v == &value),
+        "pid" => value.len() == 9,
+        "cid" => true,
+        _ => panic!("unknown field type"),
+    }
+}
+
+// check fields value
+fn passport_valid2(passport: &str) -> bool {
+    let fields = passport
+        .split_ascii_whitespace()
+        .map(|field| field.split_once(':').unwrap())
+        .collect::<HashMap<_, _>>();
+    fields.iter().all(|(k, v)| validate_field(k, v))
+}
+
 pub fn part1(input: &str) -> usize {
-    let passports = input.split("\n\n");
-    passports.filter(|p| passport_valid(p)).count()
+    input.split("\n\n").filter(|p| passport_valid1(p)).count()
 }
 
 pub fn part2(input: &str) -> usize {
-    unimplemented!()
+    input
+        .split("\n\n")
+        .filter(|p| passport_valid1(p) && passport_valid2(p))
+        .count()
 }
--- 2020/day04/day04_rust/src/main.rs
+++ 2020/day04/day04_rust/src/main.rs
@@ -3,9 +3,9 @@ use day04::{get_input, part1, part2};
 fn main() {
     let input = get_input("../input.txt");
     let part1 = part1(&input);
-    //let part2 = part2(&input);
+    let part2 = part2(&input);
     println!("Part1: {part1}");
-    //println!("Part2: {part2}");
+    println!("Part2: {part2}");
 }
 
 #[cfg(test)]
