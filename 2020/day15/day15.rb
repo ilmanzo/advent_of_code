78389ad9540bb4f325640aaadced5cdf0178c558
Andrea Manzini <ilmanzo@gmail.com>
2024-10-20 22:08:29 +0200

    2020 day15

---
+++ 2020/day15/day15.rb
@@ -0,0 +1,19 @@
+# run with input as first argument, number of rounds as second argument
+
+spoken = Hash.new { |hash, key| hash[key] = [] }
+
+ARGV.first.split(",").each.with_index(1) do |value, round|
+  spoken[value.to_i] << round
+end
+
+max_rounds = ARGV.last.to_i
+last_spoken = spoken.keys.last
+
+(spoken.size + 1).upto(max_rounds) do |round|
+  first_time = spoken[last_spoken].size == 1
+  last_spoken = first_time ? 0 : (spoken[last_spoken][-1] - spoken[last_spoken][-2])
+  spoken[last_spoken] << round
+end
+
+puts last_spoken
+
