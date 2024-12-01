2b7802a8fa2d4c1c1a90f92f592023354324c215
Andrea Manzini <ilmanzo@gmail.com>
2024-12-01 08:09:16 +0100

    shorten d1

--- 2024/day01/day01.cr
+++ 2024/day01/day01.cr
@@ -1,9 +1,6 @@
 FILENAME="input.txt"
 #FILENAME="sample.txt"
 input_pairs = File.read(FILENAME).lines.map {|l| l.chomp.split.map(&.to_i)}
-l, r = input_pairs.transpose.map(&.sort)
-p1=l.zip(r).sum {|a,b|(a - b).abs}
-t = r.tally
-p2=l.sum{|x| x * t.fetch(x, 0)}
-puts "Part1: #{p1}"
-puts "Part2: #{p2}"
+l,r = input_pairs.transpose.map(&.sort)
+p l.zip(r).sum {|a,b|(a-b).abs}
+p l.sum{|x| x * r.tally.fetch(x,0)}
