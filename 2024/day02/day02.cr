71f53c2f6265f1ea204898832ffe1c86735a79ee
Andrea Manzini <ilmanzo@gmail.com>
2024-12-02 10:29:56 +0100

    2024 day2 fix

--- 2024/day02/day02.cr
+++ 2024/day02/day02.cr
@@ -1,10 +1,10 @@
 def is_safe(row)
-   deltas=row.each_cons_pair.map{|a,b| (b-a)}
+   deltas=row.each_cons_pair.map{|a,b| (b-a)}.to_a
    deltas.all? { |n| (n>=1 && n<=3) } || deltas.all? { |n| (n>=-3 && n<=-1) }
 end
 
 FILENAME="input.txt"
 #FILENAME="sample.txt"
 input = File.read(FILENAME).lines.map {|l| l.chomp.split.map(&.to_i)}
-p input.count{|row| is_safe row}
+p input.count{|r| is_safe r}
 
