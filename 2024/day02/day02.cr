7055af6444d031b15af31bafaf5c1dac6719afb5
Andrea Manzini <ilmanzo@gmail.com>
2024-12-02 09:13:20 +0100

    day02

--- 2024/day02/day02.cr
+++ 2024/day02/day02.cr
@@ -1,1 +1,10 @@
-#TODO
+def is_safe(row)
+   deltas=row.each_cons_pair.map{|a,b| (b-a)}
+   deltas.all? { |n| (n>=1 && n<=3) } || deltas.all? { |n| (n>=-3 && n<=-1) }
+end
+
+FILENAME="input.txt"
+#FILENAME="sample.txt"
+input = File.read(FILENAME).lines.map {|l| l.chomp.split.map(&.to_i)}
+p input.count{|row| is_safe row}
+
