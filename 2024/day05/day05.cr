1638f9682f083cc440377ea5e66719b9b4f616b0
Andrea Manzini <ilmanzo@gmail.com>
2024-12-05 09:46:04 +0100

    2024 day5 part2

--- 2024/day05/day05.cr
+++ 2024/day05/day05.cr
@@ -1,19 +1,35 @@
-FILENAME="sample.txt"
-#FILENAME="input.txt"
+#FILENAME="sample.txt"
+FILENAME="input.txt"
 
 rules, pagelists = [] of Array(Int32), [] of Array(Int32)
 File.read_lines(FILENAME).each do |line|
-    rules << line.chomp.split("|").map(&.to_i).to_a if line.includes? "|"
-    pagelists << line.chomp.split(",").map(&.to_i).to_a if line.includes? ","
+    rules << line.chomp.split("|").map(&.to_i) if line.includes? "|"
+    pagelists << line.chomp.split(",").map(&.to_i) if line.includes? ","
 end
 
-part1=pagelists.map do |plist|
-    v=plist[plist.size//2]
+# returns 0 for a bad pagelist, mid value for a good one
+def value(plist,rules)
     rules.each do |rule|
         a,b=rule.map{|r| plist.index r}
         next if a.nil? || b.nil?
-        v=0 if a>=0 && b>=0 && a>b
+        return 0 if (a>=0 && b>=0 && a>b)
     end
-    v
-end.sum
-puts "part1=#{part1}"
+    plist[plist.size//2]
+end
+
+# sum median value for good page lists
+p1=pagelists.map{|p|value(p,rules)}.sum
+puts "part1=#{p1}"
+# iterate on wrong page lists
+p2=0
+pagelists.select{|p| value(p,rules)==0}.each do |p|
+    while value(p,rules)==0
+        rules.each do |rule|
+            a,b=rule.map{|r| p.index r}
+            next if a.nil? || b.nil?
+            p.swap a,b if (a>=0 && b>=0 && a>b)
+        end
+    end
+    p2+=p[p.size//2]
+end
+puts "part2=#{p2}"
