f8bfa29d819c8e95b3ef81ec802a370d159238ad
Andrea Manzini <ilmanzo@gmail.com>
2024-12-19 08:57:31 +0100

    formatting src

--- 2024/day19/day19.d
+++ 2024/day19/day19.d
@@ -5,18 +5,18 @@ string [] patterns;
 
 void main ()
 {
-	patterns=readln.strip.split (", ");
+	patterns=readln.strip.split(", ");
 	readln;
 	auto towels=stdin.byLineCopy.array;
     auto sw = StopWatch(AutoStart.yes);
-	towels.count!(t=> t.can_make).writeln;;
+	towels.count!(t=> t.can_make).writeln;
     writeln("Time for part1: ",sw.peek);
 	sw.reset();
 	towels.map!(t => t.howMany).sum.writeln;
 	writeln("Time for part2: ",sw.peek);
 }
 
-alias can_make = memoize!(can_makeImpl);
+alias can_make=memoize!(can_makeImpl);
 bool can_makeImpl(const string s) {
 	if (s.empty) return true;
 	foreach (ref p; patterns) if (s.startsWith(p) && can_make(s[p.length..$])) return true;
