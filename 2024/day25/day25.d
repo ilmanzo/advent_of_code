1b8c4345711241029e3746164930fc2062090629
Andrea Manzini <ilmanzo@gmail.com>
2024-12-25 09:21:40 +0100

    d25 2024 alternative implementation (to refine)

--- 2024/day25/day25.d
+++ 2024/day25/day25.d
@@ -10,6 +10,13 @@ pure uint match(const string[] lock, const string[] key)
     return 1;
 }
 
+// get array of lengths for each vertical stripe of '#'
+uint[5] getVertLengths(const string[] s) {
+        uint[5] result;
+        foreach (y; 0 .. 7) foreach (x; 0 .. 5) if (s[y][x] == '#') result[x]++;
+        return result;
+}
+
 void main()
 {
     string[][] locks,keys;
@@ -18,5 +25,12 @@ void main()
     auto sw=StopWatch(AutoStart.yes);
     foreach (ref l; locks) foreach (ref k; keys) part1+=match(l, k);
     writeln("Time for part1 (naive): ",sw.peek);
+    part1.writeln;
+    auto l2=locks.map!(l=>getVertLengths(l));
+    auto k2=keys.map!(k=>getVertLengths(k));
+    part1=0;
+    sw.reset();
+    foreach(l;l2) foreach(k;k2) if (l[0]+k[0]<=7 && l[1]+k[1]<=7 && l[2]+k[2]<=7 && l[3]+k[3]<=7 && l[4]+k[4]<=7) part1++;
+    writeln("Time for part1 (numeric): ",sw.peek);
     part1.writeln;
 }
